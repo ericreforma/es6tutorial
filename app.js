1dcb93870f22e358ad52f6ca4032b5840d96d748
ericreforma <ericreforma@gmail.com>
2017-10-18 11:46:17 +0800

    New commit

--- app.js
+++ app.js
@@ -3,39 +3,47 @@
 // ES6 Tutorial
 // https://www.youtube.com/watch?v=RBLIm5LMrmc&index=4&list=PLillGF-RfqbZ7s3t6ZInY3NjEOOX7hsBv
 
-// classes and inheritance
-class User{
-	constructor(username, email, password){
-		this.username = username;
-		this.email = email;
-		this.password = password;
-	}
-	register(){
-		console.log(this.username + ' is registered');
-	}
-	static countUsers(){
-		console.log('there are 50 users');
-	}
-}
-
-let NewUser = new User('Eric', 'sample@example.com', 'mypassword');
-NewUser.register();
-
-User.countUsers();
-
-class Member extends User{
-	constructor(username, email, password, myPackage){
-		super(username, email, password);
-		this.myPackage = myPackage;
+/* Classes and Inheritance */
+	class User{
+		constructor(username, email, password){
+			this.username = username;
+			this.email = email;
+			this.password = password;
+		}
+		register(){
+			console.log(this.username + ' is registered');
+		}
+		static countUsers(){
+			console.log('there are 50 users');
+		}
 	}
 
-	getPackage(){
-		console.log(this.username + ' is subscribe to standard package');
+	let NewUser = new User('Eric', 'sample@example.com', 'mypassword');
+
+	NewUser.register();
+	User.countUsers();
+
+	class Member extends User{
+		constructor(username, email, password, myPackage){
+			super(username, email, password);
+			this.myPackage = myPackage;
+		}
+
+		getPackage(){
+			console.log(this.username + ' is subscribe to standard package');
+		}
 	}
-}
 
-let eric = new Member('Eric', 'email@sample.com', 'mypassword', 'mypackage');
+	let eric = new Member('Eric', 'email@sample.com', 'mypassword', 'mypackage');
 
-eric.register();
+	eric.register();
 
-// Template LIterals
+/* Template LIterals */
+
+	// used for concatenating long strings and assiging variables inside strings
+	//instead of using single qoute use `
+
+	let name = 'John';
+	let mystring = `this is a long string,
+	with strings and another strings Hello ${name}, and simple template`;
+	console.log(mystring);
